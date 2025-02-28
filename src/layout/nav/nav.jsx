9da32cacafee16796197a100c8e50ddc9a5ce243
cyberbot501 <softwareengineerjoycy@gmail.com>
2025-02-28 04:13:51 +0100

    worked on the home

--- src/layout/nav/nav.jsx
+++ src/layout/nav/nav.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Logo from '../../assets/tech lift copy 4 1.svg';
+import React, { useState } from "react";
+import Logo from "../../assets/tech lift copy 4 1.svg";
 
 export default function Nav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,26 +14,34 @@ export default function Nav() {
 
   return (
     <div className="flex flex-row justify-between items-center px-8 bg-[#FFFFFF] h-[100px] w-[100%] overflow-hidden fixed z-10 shadow-2xl">
+    
       <img src={Logo} alt="logo" className="w-[90px] h-[90px]" />
 
-     
+      <ul className="hidden md:flex space-x-6 text-black text-[16px] font-normal">
+        <li className="cursor-pointer">Home</li>
+        <li className="cursor-pointer">About Us</li>
+        <li className="cursor-pointer">Blog</li>
+        <li className="cursor-pointer">FAQs</li>
+        <li className="cursor-pointer">Contact Us</li>
+        <li className="cursor-pointer">Sponsor Us</li>
+      </ul>
+
       <button
         onClick={toggleMenu}
-        className="md:hidden flex flex-col space-y-2"
+        className="md:hidden flex flex-col space-y-2 z-30 relative"
       >
         <div className="w-6 h-0.5 bg-black"></div>
         <div className="w-6 h-0.5 bg-black"></div>
         <div className="w-6 h-0.5 bg-black"></div>
       </button>
 
-     
+    
       <div
-        className={`fixed top-0 left-0 w-64 h-full bg-white transform ${
-          isMenuOpen ? 'translate-x-0' : '-translate-x-full'
-        } transition-transform duration-300 ease-in-out z-20`}
+        className={`fixed top-0 left-0 w-64 h-full bg-white transform md:hidden ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-500 ease-in-out z-20 shadow-lg`}
       >
         <div className="flex justify-end p-4">
-         
           <button
             onClick={closeMenu}
             className="text-black text-2xl font-bold"
@@ -42,17 +50,17 @@ export default function Nav() {
           </button>
         </div>
 
+       
         <ul className="flex flex-col space-y-4 text-black text-[16px] font-normal py-8 pl-8">
-          <li className="text-[16px] font-normal cursor-pointer">Home</li>
-          <li className="text-[16px] font-normal cursor-pointer">About Us</li>
-          <li className="text-[16px] font-normal cursor-pointer">Blog</li>
-          <li className="text-[16px] font-normal cursor-pointer">FAQs</li>
-          <li className="text-[16px] font-normal cursor-pointer">Contact Us</li>
-          <li className="text-[16px] font-normal cursor-pointer md:hidden">Sponsor Us   </li>
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">About Us</li>
+          <li className="cursor-pointer">Blog</li>
+          <li className="cursor-pointer">FAQs</li>
+          <li className="cursor-pointer">Contact Us</li>
+          <li className="cursor-pointer md:hidden">Sponsor Us</li>
         </ul>
       </div>
 
-     
       <button className="bg-[#4B0082] h-[54px] w-[128px] text-[16px] font-normal text-[#FFFFFF] rounded-[8px] cursor-pointer hidden md:block">
         Sponsor Us
       </button>
