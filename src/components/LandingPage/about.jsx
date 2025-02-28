134f7562275ed6f0cbe1e928a5bb726871b8a1fc
cyberbot501 <softwareengineerjoycy@gmail.com>
2025-02-28 04:02:24 +0100

    worked on the home

--- src/components/LandingPage/about.jsx
+++ src/components/LandingPage/about.jsx
@@ -6,9 +6,9 @@ export default function about() {
     <div className='flex flex-col justify-center items-center py-10 gap-5 px-8 md:px-20'>
       <h1 className='font-bold text-[24px] '>About Us</h1> 
       <p className='text-center md:w-[765px] font-normal '>We are a team of techies ensuring that the next generation of techies have access to the right resources and community.  we share knowledge, and empower growth through discussions resources and networking</p>
-      <div className='flex flex-col md:flex-row gap-5 items-center justify-center xl:relative bottom-56'>
+      <div className='flex flex-col md:flex-row gap-5 items-center justify-center xl:relative xl:bottom-56'>
         <div>
-            <img src={aboutimg} alt="aboutimg" className='w-[100%] h-[400px]  ' />
+            <img src={aboutimg} alt="aboutimg" className='w-[100%] h-[400px] xl:h-[800px]  ' />
         </div>
        <div className='flex flex-col gap-5'>
        <div className=''>
